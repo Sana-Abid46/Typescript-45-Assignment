4192a7c406abbe035c034659a49d867d41a2feea
Sana-Abid46 <163691895+Sana-Abid46@users.noreply.github.com>
2024-03-24 06:26:49 +0500

    Add files via upload

---
+++ 22_intentional_error.js
@@ -0,0 +1,10 @@
+"use strict";
+/* If you haven't received an array index error in one of your program yet, try to
+ make one happen.*/
+// Creating array
+let newArray = ['Maria', 'Laiba', 'Sana'];
+// Accessing array by index
+console.log(newArray[5]);
+// Make sure you correct the error before closing the program.
+// Correct the error
+console.log(newArray);
