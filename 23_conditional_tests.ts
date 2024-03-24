cad0dc0e017a7b3a485b40ecb1ef840762442edf
Sana-Abid46 <163691895+Sana-Abid46@users.noreply.github.com>
2024-03-24 06:27:15 +0500

    Add files via upload

---
+++ 23_conditional_tests.ts
@@ -0,0 +1,50 @@
+// Write a series of conditional tests.
+
+// Creating car variable
+let car : string = "subaru";
+
+// Test no 1
+console.log("Is car === 'subaru'? I predict True");
+console.log(car === 'subaru');  // True
+
+
+// Test no 2
+console.log("Is car == 'subaru'? I predict True");
+console.log(car == 'subaru');  // True
+
+
+// Test no 3
+console.log("Is car != 'honda'? I predict True");
+console.log(car != 'honda');  // True
+
+
+// Test no 4
+console.log("Is car !== 'ford'? I predict True");
+console.log(car !== 'ford');  //True
+
+
+// Test no 5
+console.log("Is car.Upper case == 'SUBARU'? I predict True");
+console.log(car.toUpperCase() == 'SUBARU'); // True
+
+// Test no 6
+console.log("Is car == 'SUBARU'? I predict True");
+console.log(car == 'SUBARU');  // False
+
+// Test no 7
+console.log("Is car === 'SUBARU'? I predict True");
+console.log(car === 'SUBARU'); // False
+
+// Test no 8
+console.log("Is car === 'Train'? I predict True");
+console.log(car === 'Train'); // False
+
+// Test no 9
+console.log("Is car == 'Bus'? I predict True");
+console.log(car == 'Bus'); // False
+
+// Test no 10
+console.log("Is car == 'Cycle'? I predict True");
+console.log(car == 'Cycle'); // False
+
+
