1cbaaefe187be9ce00a0995df5d1c51dd27a530a
Sana-Abid46 <163691895+Sana-Abid46@users.noreply.github.com>
2024-03-24 06:58:00 +0500

    Add files via upload

---
+++ 36_t-shirt.ts
@@ -0,0 +1,18 @@
+// T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it.
+
+// Call the function.
+
+function make_shirt(size:string, text:string){
+    console.log(`Creating a ${size} shirt with the message: ${text}`);
+    
+}
+make_shirt('large', 'Pakistan Zindabad');
+make_shirt('Meduim', 'Pakistan Zindabad');
+make_shirt('Small', 'Pakistan Zindabad');
+
+// Result
+
+// Creating a Large shirt with the message: Pakistan Zindabad
+// Creating a Meduim shirt with the message: Pakistan Zindabad
+// Creating a Small shirt with the message: Pakistan Zindabad
+
