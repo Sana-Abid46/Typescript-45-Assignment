3b7f73f5953a0012a46ffe3fbf5168311be1c1ff
Sana-Abid46 <163691895+Sana-Abid46@users.noreply.github.com>
2024-03-24 06:25:34 +0500

    Add files via upload

---
+++ 20_Empty_Array.js
@@ -0,0 +1,8 @@
+"use strict";
+let item = ['Pakistan', 'Urdu', 'karachi'];
+console.log(item);
+let item2 = [];
+item2.push('makkah');
+item2.push('Madina');
+item2.push('Iran');
+console.log(item2);
---
+++ 20_Empty_Array.ts
@@ -0,0 +1,11 @@
+//
+let item :string[] = ['Pakistan', 'Urdu', 'karachi'];
+console.log(item);
+
+
+let item2 :string[] = []
+
+item2.push('makkah');
+item2.push('Madina');
+item2.push('Iran');
+console.log(item2);
