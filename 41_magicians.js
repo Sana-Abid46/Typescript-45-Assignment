999bac01d827e133e71aebf73136201a1fbe6d27
Sana-Abid46 <163691895+Sana-Abid46@users.noreply.github.com>
2024-03-24 07:00:20 +0500

    Add files via upload

---
+++ 41_magicians.js
@@ -0,0 +1,9 @@
+"use strict";
+// Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array.
+let magician = ["Penn Jillette", "Teller", "Derren Brown"];
+function show_magicians(Magicians) {
+    Magicians.forEach(element => {
+        console.log(element);
+    });
+}
+show_magicians(magician);
