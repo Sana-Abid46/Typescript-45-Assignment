43e2c883778d9be2276fd79c788193000caed2d0
Sana-Abid46 <163691895+Sana-Abid46@users.noreply.github.com>
2024-03-24 06:11:07 +0500

    Add files via upload

---
+++ 14_Guest_List.ts
@@ -0,0 +1,9 @@
+
+
+let Guest_list :string[] = ['Imran Khan', 'Nawaz Sharif', 'Asif Ali Zardari'];
+
+for(let i=0; i<Guest_list.length; i++){
+
+console.log('Dear Mr.' + Guest_list[i] + '\n\nIt is our Pleasure to invite you in our party. \n\nThankYou!')
+
+}
