62e58e5b34c1b4b0e62ffe640fe07ceb8117488e
Sana-Abid46 <163691895+Sana-Abid46@users.noreply.github.com>
2024-03-24 06:23:06 +0500

    Add files via upload

---
+++ 16_More_Guest.ts
@@ -0,0 +1,36 @@
+
+
+let Guest_list :string[] = ['Imran Khan', 'Nawaz Sharif', 'Asif Ali Zardari'];
+
+// for(let i=0; i<Guest_list.length; i++){
+
+// console.log('Dear Mr.' + Guest_list[i] + '\n\nIt is our Pleasure to invite you in our party. \n\nThankYou!\n\n')
+
+// }
+
+let absent_Guest :string = 'Imran Khan' ;
+
+let new_Guest :string = 'Kamran Tessori' ;
+
+Guest_list[0] = new_Guest ;
+
+for(let i=0; i<Guest_list.length; i++){
+
+    console.log('Dear Mr.' + Guest_list[i] + '\n\nIt is our Pleasure to invite you in our party. \n\nThankYou!\n\n')
+    
+    }
+console.log(`Mr. ${absent_Guest} is not coming to the party.`);
+
+console.log('Good News! we find big table so we are inviting 3 more guest.');
+
+Guest_list.unshift('sir Zia Khan');
+
+Guest_list.splice(2 , 0 , 'Maryam Nawaz');
+
+Guest_list.push('Bilawal Bhutto Zardari');
+
+for(let i=0; i<Guest_list.length; i++){
+
+    console.log('Dear Mr.' + Guest_list[i] + '\n\nIt is our Pleasure to invite you in our party. \n\nThankYou!\n\n')
+    
+    }
