516171b5afacee50f91ff1d1322312960e79cf1d
Sana-Abid46 <163691895+Sana-Abid46@users.noreply.github.com>
2024-03-24 06:37:55 +0500

    Add files via upload

---
+++ 31_no_user.js
@@ -0,0 +1,9 @@
+"use strict";
+let userNames = ['admin', 'sana', 'sadia', 'sara', 'saima'];
+if (userNames.length === 0) {
+    console.log('We need to find some users!');
+}
+else {
+    userNames = [];
+    console.log('All user names have been removed. ' + userNames.length);
+}
