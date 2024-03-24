f5ccce35bbbea64d0443e344d9f3919ce7e35aed
Sana-Abid46 <163691895+Sana-Abid46@users.noreply.github.com>
2024-03-24 06:09:45 +0500

    Add files via upload

---
+++ 12_Greetings.js
@@ -0,0 +1,6 @@
+"use strict";
+let names = ['Sana', 'Sadiq', 'Madani'];
+const message = 'Welcome to My Project: ';
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
---
+++ 12_Greetings.ts
@@ -0,0 +1,10 @@
+
+
+let names :string[] = ['Sana', 'Sadiq', 'Madani'];
+
+const message :string = 'Welcome to My Project: ';
+
+for(let i=0; i<names.length; i++){
+
+    console.log(message + names[i]);
+}
